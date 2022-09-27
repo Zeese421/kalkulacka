22564e1f3fb05a4ae7295d3b24a5105311b09700
Franke Max <Franke.Ma.2022@skola.ssps.cz>
2022-09-27 09:30:14 +0200

    first init

---
+++ Program/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            double cislo1, cislo2, vysledek = 0;
+            string operace;
+
+                cislo1 = double.Parse(Console.ReadLine());
+            cislo2 = double.Parse(Console.ReadLine());
+            operace = Console.ReadLine();
+
+            if (operace == "+")
+            {
+                vysledek = cislo1 + cislo2;
+            }
+        
+            else if (operace == "-")
+            {
+                vysledek = cislo1 - cislo2;
+            }
+
+            else if(operace == "*")
+            {
+                vysledek = cislo1 * cislo2;
+            }
+
+            else if(operace == "/")
+            {
+                vysledek = cislo1 / cislo2;
+            }
+            
+            else
+            {
+                Console.WriteLine("špatná operace bozo");
+
+            }
+
+
+            Console.WriteLine("Číslo: " + vysledek);
+
+                Console.ReadKey();
+
+                    
+        }
+    }
+}
